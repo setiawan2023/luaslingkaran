cd1a08f3334bbd55436d7a143cf7f873cad5fb71
setiawan2023 <151593560+setiawan2023@users.noreply.github.com>
2023-12-13 16:16:17 +0700

    Add files via upload
    ujian :agung,alya

---
+++ luaslingkaran agung.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+     float r, luas;
+ 
+  cout << "Input jari-jari lingkaran: ";
+  cin >> r;
+  luas = 3.14 * r * r;
+ 
+  cout << "Luas lingkaran = "<< luas << endl;
+ 
+  return 0;
+}
